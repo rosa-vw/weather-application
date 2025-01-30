eb0897ea22bef0ffd51aa52890963e3441f2752a
Rosa van Wouwe <rosavanwouwe@hotmail.com>
2025-01-30 15:51:12 +0100

    Formatting date & getting other details from api
    Now updates time and date, perceived temp, humidity and wind speed.

--- source/script.js
+++ source/script.js
@@ -1,9 +1,74 @@
 function updateWeather(response) {
   let temperature = document.querySelector("#temperature");
   let currentTemperature = Math.round(response.data.temperature.current);
+  temperature.innerHTML = `${currentTemperature}°C`;
   let city = document.querySelector("#city");
   city.innerHTML = response.data.city;
-  temperature.innerHTML = `${currentTemperature}°C`;
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.condition.description;
+  let perceived = document.querySelector("#perceived");
+  let currentPerceived = Math.round(response.data.temperature.feels_like);
+  perceived.innerHTML = `${currentPerceived}°C`;
+  let humidity = document.querySelector("#humidity");
+  let currentHumidity = response.data.temperature.humidity;
+  humidity.innerHTML = `${currentHumidity}%`;
+  let windSpeed = document.querySelector("#wind-speed");
+  let currentWindSpeed = Math.round(response.data.wind.speed);
+  windSpeed.innerHTML = `${currentWindSpeed} KM/H`;
+  let date = new Date(response.data.time * 1000);
+  let time = document.querySelector("#time");
+  time.innerHTML = formatDay(date);
+  let calendar = document.querySelector("#calendar");
+  calendar.innerHTML = formatDate(date);
+}
+
+function formatDay(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDate(date) {
+  let day = date.getDate();
+  let year = date.getFullYear();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  return `${month} ${day}, ${year}`;
 }
 
 function searchCity(city) {
